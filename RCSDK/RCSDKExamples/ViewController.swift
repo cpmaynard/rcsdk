1763b3e339d904eb0c48012612dfcb1edc29668b
Rakesh Palotra <ninjacoderrakesh@gmail.com>
2020-04-29 01:29:45 +0530

    Added spec file

--- RCSDK/RCSDKExamples/ViewController.swift
+++ RCSDK/RCSDKExamples/ViewController.swift
@@ -7,22 +7,13 @@
 //
 
 import UIKit
-import RCSDK
+
 class ViewController: UIViewController {
 
-     var widget:RCJSWidgetView?
        override func viewDidLoad() {
-           super.viewDidLoad()
-           RCSDK.setup()
-           self.widget = RCJSWidgetView.init(frame: CGRect.init(x: 0.0, y: 270, width: self.view.frame.size.width, height: self.view.frame.size.height-270))
-           let subIds = ["category":"BBQ","utm_code":"123456"]
-           self.widget!.setWidgetId(widgetId: "66620")
-           self.widget!.setWidgetSubId(widgetSubId:subIds)
-           self.widget!.loadWidget()
-           self.view.addSubview(self.widget!)
+        
        }
        @IBAction func actionBackButton(sender: UIButton) {
-           self.widget!.loadWidget()
        }
 
 }
